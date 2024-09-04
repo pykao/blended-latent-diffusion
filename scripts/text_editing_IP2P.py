0e3053f3b022eaa5a2a0b884678f643a8693acef
Po-Yu Kao <poyukao@google.com>
2024-09-04 11:22:51 +0800

    added official example image and prompt

--- scripts/text_editing_IP2P.py
+++ scripts/text_editing_IP2P.py
@@ -1,9 +1,19 @@
 import argparse
 import numpy as np
+import requests
+import PIL
 from PIL import Image
 
 from diffusers import StableDiffusionInstructPix2PixPipeline
+from diffusers import EulerAncestralDiscreteScheduler
 import torch
+
+url = "https://raw.githubusercontent.com/timothybrooks/instruct-pix2pix/main/imgs/example.jpg"
+def download_image(url):
+    image = PIL.Image.open(requests.get(url, stream=True).raw)
+    image = PIL.ImageOps.exif_transpose(image)
+    image = image.convert("RGB")
+    return image
 
 
 class BlendedLatnetDiffusion:
@@ -41,6 +51,7 @@ class BlendedLatnetDiffusion:
             self.args.model_path,
             torch_dtype=torch.float16,
         ).to(self.args.device)
+        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
 
 
     @torch.no_grad()
@@ -50,13 +61,15 @@ class BlendedLatnetDiffusion:
         prompt,
         height=512,
         width=512,
-        num_inference_steps=50,
-        image_guidance_scale=7.5,
+        num_inference_steps=10,
+        image_guidance_scale=1,
     ):
 
         image = Image.open(image_path)
         image = image.resize((height, width), Image.BILINEAR)
         image = np.array(image)[:, :, :3]
+        image = download_image(url)
+        prompt = "van Gogh"
         image = self.pipe(prompt=prompt, image=image, num_inference_steps=num_inference_steps, image_guidance_scale=image_guidance_scale).images[0]
 
         return image
