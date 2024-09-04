3cbb6a2fdf4db634af80338a61fffda41ef3973d
Po-Yu Kao <poyukao@google.com>
2024-09-04 11:27:20 +0800

    added different masks

--- run_examples_bld_IP2P.sh
+++ run_examples_bld_IP2P.sh
@@ -14,12 +14,16 @@ for file in "$input_folder"/*_image_condition.png; do
 
   # Construct the full paths for init_image, mask, and output_path
   init_image="$input_folder/${base_name}._image_condition.png"
+  #mask="$input_folder/${base_name}._mask_condition.png"
+  mask="$input_folder/mask-all.png"
   output_path="outputs/${base_name}._output.png"
 
   # Run the Python script with the extracted filenames
   python scripts/text_editing_bld_IP2P.py \
-      --prompt "chinese ink" \
+      --prompt "van Gogh" \
       --init_image "$init_image" \
+      --mask "$mask" \
       --output_path "$output_path" \
+      --batch_size 1\
 
 done
